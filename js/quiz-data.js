d0fe34435f03baa33cd05a48e279d314b6d5a4fe
Nichkov Roman <nichkovroman@gmail.com>
2020-11-18 13:47:37 +0300

    js
    show question, next one (btn click)

---
+++ js/quiz-data.js
@@ -0,0 +1,46 @@
+const quiz = [
+    {
+        'question': 'Столица Италии: ',
+        'image': 'images/italy.jpg',
+        'answer': 'РИМ'
+    },
+
+    {
+        'question': 'Столица России: ',
+        'image': 'images/russia.jpg',
+        'answer': 'МОСКВА'
+    },
+
+    {
+        'question': 'Столица Великобритании: ',
+        'image': 'images/uk.jpg',
+        'answer': 'ЛОНДОН'
+    },
+
+    {
+        'question': 'Столица Франции: ',
+        'image': 'images/france.jpg',
+        'answer': 'ПАРИЖ'
+    },
+
+    {
+        'question': 'Столица Германии: ',
+        'image': 'images/germany.jpg',
+        'answer': 'БЕРЛИН'
+    },
+
+    {
+        'question': 'Столица Нидерландов: ',
+        'image': 'images/netherlands.jpg',
+        'answer': 'АМСТЕРДАМ'
+    },
+    
+    {
+        'question': 'Столица Португалии: ',
+        'image': 'images/portugal.jpg',
+        'answer': 'ЛИССАБОН'
+    },
+
+  
+
+];
