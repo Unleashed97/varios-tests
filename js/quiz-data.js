f2d9daa50d40c989a0b4f89c1363ca8aa1186a21
Nichkov Roman <nichkovroman@gmail.com>
2021-02-16 15:41:01 +0300

    fix

--- js/quiz-data.js
+++ js/quiz-data.js
@@ -30,241 +30,241 @@ const quiz = [
         'answer': 'ДУБЛИН'
     },
 
-    // {
-    //     'question': 'Столица Лихтенштейна: ',
-    //     'image': 'images/liechtenstein.jpg',
-    //     'answer': 'ВАДУЦ'
-    // },
+    {
+        'question': 'Столица Лихтенштейна: ',
+        'image': 'images/liechtenstein.jpg',
+        'answer': 'ВАДУЦ'
+    },
 
-    // {
-    //     'question': 'Столица Люксембурга: ',
-    //     'image': 'images/luxemburg.jpg',
-    //     'answer': 'ЛЮКСЕМБУРГ'
-    // },
+    {
+        'question': 'Столица Люксембурга: ',
+        'image': 'images/luxemburg.jpg',
+        'answer': 'ЛЮКСЕМБУРГ'
+    },
 
-    // {
-    //     'question': 'Столица Монако: ',
-    //     'image': 'images/monaco.jpg',
-    //     'answer': 'МОНАКО'
-    // },
+    {
+        'question': 'Столица Монако: ',
+        'image': 'images/monaco.jpg',
+        'answer': 'МОНАКО'
+    },
 
-    // {
-    //     'question': 'Столица Нидерландов: ',
-    //     'image': 'images/netherlands.jpg',
-    //     'answer': 'АМСТЕРДАМ'
-    // },
+    {
+        'question': 'Столица Нидерландов: ',
+        'image': 'images/netherlands.jpg',
+        'answer': 'АМСТЕРДАМ'
+    },
 
-    // {
-    //     'question': 'Столица Франции: ',
-    //     'image': 'images/france.jpg',
-    //     'answer': 'ПАРИЖ'
-    // },
+    {
+        'question': 'Столица Франции: ',
+        'image': 'images/france.jpg',
+        'answer': 'ПАРИЖ'
+    },
 
-    // {
-    //     'question': 'Столица Швейцарии: ',
-    //     'image': 'images/switzerland.jpg',
-    //     'answer': 'БЕРН'
-    // },
+    {
+        'question': 'Столица Швейцарии: ',
+        'image': 'images/switzerland.jpg',
+        'answer': 'БЕРН'
+    },
 
-    // // Eastern Europe
-    // {
-    //     'question': 'Столица Беларуси: ',
-    //     'image': 'images/belarus.jpg',
-    //     'answer': 'МИНСК'
-    // },
+    // Eastern Europe
+    {
+        'question': 'Столица Беларуси: ',
+        'image': 'images/belarus.jpg',
+        'answer': 'МИНСК'
+    },
 
-    // {
-    //     'question': 'Столица Болгарии: ',
-    //     'image': 'images/bulgaria.jpg',
-    //     'answer': 'СОФИЯ'
-    // },
+    {
+        'question': 'Столица Болгарии: ',
+        'image': 'images/bulgaria.jpg',
+        'answer': 'СОФИЯ'
+    },
 
-    // {
-    //     'question': 'Столица Венгрии: ',
-    //     'image': 'images/hungary.jpg',
-    //     'answer': 'БУДАПЕШТ'
-    // },
+    {
+        'question': 'Столица Венгрии: ',
+        'image': 'images/hungary.jpg',
+        'answer': 'БУДАПЕШТ'
+    },
 
-    // {
-    //     'question': 'Столица Молдавии: ',
-    //     'image': 'images/moldova.jpg',
-    //     'answer': 'КИШЕНЕВ'
-    // },
+    {
+        'question': 'Столица Молдавии: ',
+        'image': 'images/moldova.jpg',
+        'answer': 'КИШЕНЕВ'
+    },
 
-    // {
-    //     'question': 'Столица Польши: ',
-    //     'image': 'images/poland.jpg',
-    //     'answer': 'ВАРШАВА'
-    // },
+    {
+        'question': 'Столица Польши: ',
+        'image': 'images/poland.jpg',
+        'answer': 'ВАРШАВА'
+    },
 
-    // {
-    //     'question': 'Столица России: ',
-    //     'image': 'images/russia.jpg',
-    //     'answer': 'МОСКВА'
-    // },
+    {
+        'question': 'Столица России: ',
+        'image': 'images/russia.jpg',
+        'answer': 'МОСКВА'
+    },
 
-    // {
-    //     'question': 'Столица Румынии: ',
-    //     'image': 'images/romania.jpg',
-    //     'answer': 'БУХАРЕСТ'
-    // },
+    {
+        'question': 'Столица Румынии: ',
+        'image': 'images/romania.jpg',
+        'answer': 'БУХАРЕСТ'
+    },
 
-    // {
-    //     'question': 'Столица Словакии: ',
-    //     'image': 'images/slovakia.jpg',
-    //     'answer': 'БРАТИСЛАВА'
-    // },
+    {
+        'question': 'Столица Словакии: ',
+        'image': 'images/slovakia.jpg',
+        'answer': 'БРАТИСЛАВА'
+    },
 
-    // {
-    //     'question': 'Столица Чехии: ',
-    //     'image': 'images/czech.jpg',
-    //     'answer': 'ПРАГА'
-    // },
+    {
+        'question': 'Столица Чехии: ',
+        'image': 'images/czech.jpg',
+        'answer': 'ПРАГА'
+    },
 
-    // {
-    //     'question': 'Столица Украины: ',
-    //     'image': 'images/ukraine.jpg',
-    //     'answer': 'КИЕВ'
-    // },
+    {
+        'question': 'Столица Украины: ',
+        'image': 'images/ukraine.jpg',
+        'answer': 'КИЕВ'
+    },
 
-    // //North Europe
-    // {
-    //     'question': 'Столица Дании: ',
-    //     'image': 'images/denmark.jpg',
-    //     'answer': 'КОПЕНГАГЕН'
-    // },
+    //North Europe
+    {
+        'question': 'Столица Дании: ',
+        'image': 'images/denmark.jpg',
+        'answer': 'КОПЕНГАГЕН'
+    },
 
-    // {
-    //     'question': 'Столица Исландии: ',
-    //     'image': 'images/iceland.jpg',
-    //     'answer': 'РЕЙКЬЯВИК'
-    // },
+    {
+        'question': 'Столица Исландии: ',
+        'image': 'images/iceland.jpg',
+        'answer': 'РЕЙКЬЯВИК'
+    },
 
-    // {
-    //     'question': 'Столица Латвии: ',
-    //     'image': 'images/latvia.jpg',
-    //     'answer': 'РИГА'
-    // },
+    {
+        'question': 'Столица Латвии: ',
+        'image': 'images/latvia.jpg',
+        'answer': 'РИГА'
+    },
 
-    // {
-    //     'question': 'Столица Литвы: ',
-    //     'image': 'images/lithuania.jpg',
-    //     'answer': 'ВИЛЬНЮС'
-    // },
+    {
+        'question': 'Столица Литвы: ',
+        'image': 'images/lithuania.jpg',
+        'answer': 'ВИЛЬНЮС'
+    },
 
-    // {
-    //     'question': 'Столица Норвегии: ',
-    //     'image': 'images/norway.jpg',
-    //     'answer': 'ОСЛО'
-    // },
+    {
+        'question': 'Столица Норвегии: ',
+        'image': 'images/norway.jpg',
+        'answer': 'ОСЛО'
+    },
 
-    // {
-    //     'question': 'Столица Финляндии: ',
-    //     'image': 'images/finland.jpg',
-    //     'answer': 'ХЕЛЬСИНКИ'
-    // },
+    {
+        'question': 'Столица Финляндии: ',
+        'image': 'images/finland.jpg',
+        'answer': 'ХЕЛЬСИНКИ'
+    },
 
-    // {
-    //     'question': 'Столица Эстонии: ',
-    //     'image': 'images/estonia.jpg',
-    //     'answer': 'ТАЛЛИН'
-    // },
+    {
+        'question': 'Столица Эстонии: ',
+        'image': 'images/estonia.jpg',
+        'answer': 'ТАЛЛИН'
+    },
 
-    // {
-    //     'question': 'Столица Швеция: ',
-    //     'image': 'images/sweden.jpg',
-    //     'answer': 'СТОКГОЛЬМ'
-    // },
+    {
+        'question': 'Столица Швеция: ',
+        'image': 'images/sweden.jpg',
+        'answer': 'СТОКГОЛЬМ'
+    },
 
-    // // South Europe
-    // {
-    //     'question': 'Столица Албании: ',
-    //     'image': 'images/albania.jpg',
-    //     'answer': 'ТИРАНА'
-    // },
+    // South Europe
+    {
+        'question': 'Столица Албании: ',
+        'image': 'images/albania.jpg',
+        'answer': 'ТИРАНА'
+    },
 
-    // {
-    //     'question': 'Столица Андорры: ',
-    //     'image': 'images/andorra.jpg',
-    //     'answer': 'АНДОРРА-ЛА-ВЕЛЬЯ'
-    // },
+    {
+        'question': 'Столица Андорры: ',
+        'image': 'images/andorra.jpg',
+        'answer': 'АНДОРРА-ЛА-ВЕЛЬЯ'
+    },
 
-    // {
-    //     'question': 'Столица Боснии и Герцеговины: ',
-    //     'image': 'images/big.jpg',
-    //     'answer': 'САРАЕВО'
-    // },
+    {
+        'question': 'Столица Боснии и Герцеговины: ',
+        'image': 'images/big.jpg',
+        'answer': 'САРАЕВО'
+    },
 
-    // {
-    //     'question': 'Столица Ватикана: ',
-    //     'image': 'images/vatican.jpg',
-    //     'answer': 'ВАТИКАН'
-    // },
+    {
+        'question': 'Столица Ватикана: ',
+        'image': 'images/vatican.jpg',
+        'answer': 'ВАТИКАН'
+    },
 
-    // {
-    //     'question': 'Столица Греции: ',
-    //     'image': 'images/greece.jpg',
-    //     'answer': 'АФИНЫ'
-    // },
+    {
+        'question': 'Столица Греции: ',
+        'image': 'images/greece.jpg',
+        'answer': 'АФИНЫ'
+    },
 
-    // {
-    //     'question': 'Столица Испании: ',
-    //     'image': 'images/spain.jpg',
-    //     'answer': 'МАДРИД'
-    // },
+    {
+        'question': 'Столица Испании: ',
+        'image': 'images/spain.jpg',
+        'answer': 'МАДРИД'
+    },
 
-    // {
-    //     'question': 'Столица Италии: ',
-    //     'image': 'images/italy.jpg',
-    //     'answer': 'РИМ'
-    // },
+    {
+        'question': 'Столица Италии: ',
+        'image': 'images/italy.jpg',
+        'answer': 'РИМ'
+    },
 
-    // {
-    //     'question': 'Столица Северной Македонии: ',
-    //     'image': 'images/north-macedonia.jpg',
-    //     'answer': 'СКОПЬЕ'
-    // },
+    {
+        'question': 'Столица Северной Македонии: ',
+        'image': 'images/north-macedonia.jpg',
+        'answer': 'СКОПЬЕ'
+    },
 
-    // {
-    //     'question': 'Столица Мальты: ',
-    //     'image': 'images/malta.jpg',
-    //     'answer': 'ВАЛЛЕТТА'
-    // },
+    {
+        'question': 'Столица Мальты: ',
+        'image': 'images/malta.jpg',
+        'answer': 'ВАЛЛЕТТА'
+    },
 
-    // {
-    //     'question': 'Столица Португалии: ',
-    //     'image': 'images/portugal.jpg',
-    //     'answer': 'ЛИССАБОН'
-    // },
+    {
+        'question': 'Столица Португалии: ',
+        'image': 'images/portugal.jpg',
+        'answer': 'ЛИССАБОН'
+    },
 
-    // {
-    //     'question': 'Столица Сан Марино: ',
-    //     'image': 'images/san-marino.jpg',
-    //     'answer': 'САН-МАРИНО'
-    // },
+    {
+        'question': 'Столица Сан Марино: ',
+        'image': 'images/san-marino.jpg',
+        'answer': 'САН-МАРИНО'
+    },
 
-    // {
-    //     'question': 'Столица Сербии: ',
-    //     'image': 'images/serbia.jpg',
-    //     'answer': 'БЕЛГРАД'
-    // },
+    {
+        'question': 'Столица Сербии: ',
+        'image': 'images/serbia.jpg',
+        'answer': 'БЕЛГРАД'
+    },
 
-    // {
-    //     'question': 'Столица Словении: ',
-    //     'image': 'images/slovenia.jpg',
-    //     'answer': 'ЛЮБЛЯНА'
-    // },
+    {
+        'question': 'Столица Словении: ',
+        'image': 'images/slovenia.jpg',
+        'answer': 'ЛЮБЛЯНА'
+    },
 
-    // {
-    //     'question': 'Столица Хорватии: ',
-    //     'image': 'images/croatia.jpg',
-    //     'answer': 'ЗАГРЕБ'
-    // },
+    {
+        'question': 'Столица Хорватии: ',
+        'image': 'images/croatia.jpg',
+        'answer': 'ЗАГРЕБ'
+    },
 
-    // {
-    //     'question': 'Столица Черногории: ',
-    //     'image': 'images/montenegro.jpg',
-    //     'answer': 'ПОДГОРИЦА'
-    // },
+    {
+        'question': 'Столица Черногории: ',
+        'image': 'images/montenegro.jpg',
+        'answer': 'ПОДГОРИЦА'
+    },
 
 ];
